9528b507fcdff6ddad1cdd3349b5ec5cd699f010
khairunnisamuhardi <khairunnisa.muhardi@gmail.com>
2026-04-06 15:19:24 +0700

    Praktikum Pekan 2

---
+++ PrakSD2026_C_2511532005/src/pekan2_2511532005/ArrayList1_2511532005.java
@@ -0,0 +1,24 @@
+package pekan2_2511532005;
+import java.util.ArrayList;
+
+public class ArrayList1_2511532005 {
+	public static void main (String[] args) {
+		// size of the ArrayList
+		int n = 5;
+		// Declaring the ArrayList with initial size n
+		ArrayList<Integer> arrli = new ArrayList<Integer>(n);
+		// Appending new elements at the end of the List
+		for (int i = 1; i <= n; i++)
+			arrli.add(i);
+		// printing elements
+		System.out.println(arrli);
+		// remove element at index 3
+		arrli.remove(3);
+		// Displaying the ArrayList
+		// after deletion
+		System.out.println(arrli);
+		//printing elements one by one
+		for (int i = 0; i < arrli.size(); i++)
+		System.out.print(arrli.get(i) + " ");
+	}
+}
